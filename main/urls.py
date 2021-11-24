9fe8b97ac01eb9dd2d8f46af9e3d0eaad8919a06
progenor <nero6126@gmail.com>
2021-11-24 17:23:25 +0200

    asd

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("create/", views.create, name="create"),
-    path("<int:id>", views.index, name="index"),
     path("view/", views.view, name="view"),
 ]
